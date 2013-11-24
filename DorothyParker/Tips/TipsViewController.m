a9ab5e6944a52323afd3c62a66c96d1531132321
Janice <janicegaringo@gmail.com>
2013-11-24 14:36:04 -0500

    Adding styling to tips

--- DorothyParker/Tips/TipsViewController.m
+++ DorothyParker/Tips/TipsViewController.m
@@ -26,11 +26,12 @@
 
 - (void)styleView
 {
-    self.view.backgroundColor = [UIColor darkGrayColor];
+    self.view.backgroundColor = [UIColor whiteColor];
     
     self.navigationController.navigationBar.translucent = NO;
-    self.navigationController.navigationBar.barTintColor = kUIColorDarkGrey;
-    self.navigationItem.title = @"Tips";
+    self.navigationController.navigationBar.barTintColor = kUIColorMedGrey;
+    
+    self.navigationItem.title = @"Mixing Tips and Tricks";
     [self.navigationController.navigationBar setTitleTextAttributes: [NSDictionary dictionaryWithObjectsAndKeys:
                                                                       kNovellaFont, NSFontAttributeName,
                                                                       [UIColor whiteColor],NSForegroundColorAttributeName,
@@ -38,6 +39,7 @@
     
     [self.webView setBackgroundColor:[UIColor clearColor]];
     [self.webView setOpaque:NO];
+  
 }
 
 
@@ -49,14 +51,17 @@
     AFJSONRequestOperation *operation = [AFJSONRequestOperation JSONRequestOperationWithRequest:request success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
         
         NSDictionary *dictionary = (NSDictionary *) JSON;
-        
         NSArray *data = [dictionary objectForKey:@"data"];
+        
         [data enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
             
             NSString *dataLoaded = [obj objectForKey:@"tips"];
-            NSString *htmlString = [NSString stringWithFormat:@"<font color='white'>%@</font>", dataLoaded];
-            [self.webView loadHTMLString:htmlString baseURL:nil];
+            NSString *path = [[NSBundle mainBundle] pathForResource: @"webView" ofType: @"html"];
+            NSError *error;
+            NSString *html = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:&error];
+            html = [html stringByReplacingOccurrencesOfString:@"<!-- body -->" withString:dataLoaded];
             
+             [self.webView loadHTMLString:html baseURL:nil];
         }];
         
     } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON) {
